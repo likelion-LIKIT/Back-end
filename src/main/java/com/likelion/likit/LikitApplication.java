1f0fb329167f389acbd1663b1df62d8ef9e8f45a
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-17 15:07:04 +0900

    feat(init) : create a new project

---
+++ src/main/java/com/likelion/likit/LikitApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.likit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/likit/LikitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.likit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
