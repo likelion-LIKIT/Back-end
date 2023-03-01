887b942c887a1d0307321a42b80c2aa335d23585
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2023-03-01 18:31:26 +0900

    style(Member) : change FRONTEND to FRONT_END

--- src/main/java/com/likelion/likit/member/entity/Track.java
+++ src/main/java/com/likelion/likit/member/entity/Track.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum Track {
     PM_DESIGN("PM_DESIGN"),
-    FRONT_END("FRONTEND"),
+    FRONT_END("FRONT_END"),
     BACK_END("BACK_END"),
     MOBILE("MOBILE");
     private String track;
