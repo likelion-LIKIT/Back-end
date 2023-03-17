842b5b2fcd3d6c55b489b4c6b8bc850149936566
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2023-03-18 00:42:37 +0900

    Fix(Diary) : change postmapping return

--- src/main/java/com/likelion/likit/diary/DiaryController.java
+++ src/main/java/com/likelion/likit/diary/DiaryController.java
@@ -1,14 +1,13 @@
 package com.likelion.likit.diary;
 
 import com.likelion.likit.diary.dto.DiaryReqDto;
-import com.likelion.likit.diary.dto.DiaryResDto;
+import com.likelion.likit.diary.dto.DiaryThumbnailDto;
 import com.likelion.likit.diary.entity.Diary;
 import com.likelion.likit.member.MemberController;
 import com.likelion.likit.member.entity.Member;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,11 +22,13 @@ public class DiaryController {
 
     @Operation(summary = "diary 글 작성", description = "성공하면 게시글이 Diary 데이터베이스에 저장")
     @PostMapping("/diary")
-    public ResponseEntity<DiaryResDto> create(@RequestHeader String accessToken,
+    public ResponseEntity<Integer> create(@RequestHeader String accessToken,
                                               @RequestPart(value = "diaryReqDto") DiaryReqDto diaryReqDto,
+                                              @RequestPart(value = "thumbnail")List<MultipartFile> thumbnail,
                                               @RequestPart(value = "file", required = false)List<MultipartFile> files) throws Exception {
         Member member = memberController.findMemberByToken(accessToken);
-        return ResponseEntity.ok(diaryService.create(member, diaryReqDto, files));
+        diaryService.create(member, diaryReqDto, thumbnail, files);
+        return ResponseEntity.ok(1);
     }
 
     @Operation(summary = "diary 글 조회", description = "Diary 글 조희")
@@ -36,12 +37,11 @@ public class DiaryController {
         return diaryService.viewDiary();
     }
 
-//    @Operation(summary = "diary 글 작성", description = "성공하면 게시글이 Diary 데이터베이스에 저장")
-//    @PostMapping("/diary")
-//    public ResponseEntity<DiaryResDto> create(@RequestHeader String accessToken,
-//                                              @RequestPart(value = "diaryReqDto") DiaryReqDto diaryReqDto,
-//                                              @RequestPart(value = "file", required = false)List<MultipartFile> files) throws Exception {
-//        Member member = memberController.findMemberByToken(accessToken);
-//        return ResponseEntity.ok(diaryService.create(member, diaryReqDto, files));
-//    }
+    @Operation(summary = "diary 글 조회", description = "Diary 글 조희")
+    @GetMapping("/diary/thubnail")
+    public List<DiaryThumbnailDto> viewDiaryWithThubnail() {
+        return diaryService.viewDiaryWithThumbnail();
+    }
+
+
 }
