e2bcf5c06f93b358bc3995b6fb975b25f96a217a
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2023-03-24 06:16:05 +0900

    feat(Notice) : create dto of notice

---
+++ src/main/java/com/likelion/likit/notice/dto/NoticeReqDto.java
@@ -0,0 +1,40 @@
+package com.likelion.likit.notice.dto;
+
+import com.likelion.likit.notice.entity.Category;
+import com.likelion.likit.notice.entity.Notice;
+import com.likelion.likit.notice.entity.NoticeFile;
+import com.likelion.likit.member.entity.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NoticeReqDto {
+    private String title;
+    private String description;
+    private String location;
+    private Category category;
+    private String date;
+
+    @Builder
+    public NoticeReqDto(Notice notice) {
+        this.title = notice.getTitle();
+        this.description = notice.getDescription();
+        this.location = notice.getLocation();
+        this.category = notice.getCategory();
+        this.date = notice.getDate();
+    }
+
+    public Notice toEntity(Member member, NoticeFile thumbnail) {
+        return Notice.builder()
+                .member(member)
+                .thumbnail(thumbnail)
+                .title(title)
+                .description(description)
+                .location(location)
+                .category(category)
+                .date(date)
+                .build();
+    }
+}
