63188c6bc53eb1c539eeb8a501c6b42563cff50a
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2023-04-01 02:06:41 +0900

    fix(Config) : fix cors error

--- src/main/java/com/likelion/likit/config/SecurityConfig.java
+++ src/main/java/com/likelion/likit/config/SecurityConfig.java
@@ -96,10 +96,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    //allowedOriginPatterns
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:3000", "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
